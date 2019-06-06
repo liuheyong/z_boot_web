03a8654f96aafa0fcaa935283bbf0f785e2d5118
liuheyong <17682347237@163.com>
2019-06-06 15:54:53 +0800

    commit

---
+++ src/main/java/com/boot/web/bootenum/Events.java
@@ -0,0 +1,13 @@
+package com.boot.web.bootenum;
+
+/**
+ * @author: LiuHeYong
+ * @create: 2019-06-06
+ * @description: 事件枚举
+ **/
+public enum Events {
+
+    PAY,        // 支付
+    RECEIVE     // 收货
+
+}
---
+++ src/main/java/com/boot/web/bootenum/States.java
@@ -0,0 +1,14 @@
+package com.boot.web.bootenum;
+
+/**
+ * @author: LiuHeYong
+ * @create: 2019-06-06
+ * @description: 订单状态
+ **/
+public enum  States {
+
+    UNPAID,                 // 待支付
+    WAITING_FOR_RECEIVE,    // 待收货
+    DONE                    // 结束
+
+}
