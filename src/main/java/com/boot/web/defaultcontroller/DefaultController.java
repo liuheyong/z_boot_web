a7c8ff8c5f42d302aa01f526c0f2c6b0a114964c
liuheyong <17682347237@163.com>
2019-06-09 01:21:08 +0800

    commit

--- src/main/java/com/boot/web/defaultcontroller/DefaultController.java
+++ src/main/java/com/boot/web/defaultcontroller/DefaultController.java
@@ -1,15 +1,9 @@
 package com.boot.web.defaultcontroller;
 
-import com.boot.commons.dto.Student;
-import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author: LiuHeYong
@@ -23,11 +17,5 @@ public class DefaultController implements Cloneable, Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    public static Student student01 = new Student("liu", 91);
-    public static Student student02 = new Student("wang", 92);
-    public static Student student03 = new Student("huang", 93);
-    public static Student student04 = new Student("han", 94);
-    public static Student student05 = new Student("han", 94);
-    public static List<Student> studentList = Lists.newArrayList(student01, student02, student03, student04, student05);
 
 }
